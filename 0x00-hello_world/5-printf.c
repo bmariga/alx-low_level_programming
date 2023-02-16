a1fc8d0970994d56b8349b08078856eeaa042fdd
Bruce Mariga <mariga.bn@gmail.com>
2023-02-16 23:08:48 +0300

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main - this is the main function
+ * Description: Prints
+ *
+ * Return: 0
+*/
+
 int main(void) {
 	printf("\"ith proper grammar, but the outcome is a piece of art,\n");
 	return 0;
