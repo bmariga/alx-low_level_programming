44cb01bbe18f04be76ce4ecfd99f74cf0ff018bb
Bruce Mariga <mariga.bn@gmail.com>
2023-02-16 22:56:59 +0300

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main - prints the text "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
+ *
+ */
+
 int main(void) {
 	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
