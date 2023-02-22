80344528915e9236d4790eefc694c92a0a5fd932
Bruce Mariga <mariga.bn@gmail.com>
2023-02-22 19:31:17 +0300

    C Functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: always (0) success
+*/
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char ch;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of a number
+ * @n: number to ckeck
+ *
+ * Return: last digit
+*/
+
+int print_last_digit(int n)
+{
+	int num;
+
+	if (n < 0)
+	{
+		num = -1 * (n % 10);
+	} else
+	{
+		num = n % 10;
+	}
+	_putchar(num + '0');
+	return (num);
+}
