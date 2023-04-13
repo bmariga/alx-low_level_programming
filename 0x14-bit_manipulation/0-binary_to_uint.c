db094dfe45306c89bd0244b0c5785dc1cd58f349
mariga bruce <mariga.bn@gmail.com>
2023-04-13 22:16:40 +0300

    Task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,37 @@
-#include <stdio.h>
-#include <string.h>
+#include "main.h"
 
-unsigned int binary_to_unsigned_int(const char *binary_str) {
-    // Get the length of the input string
-    int len = strlen(binary_str);
+/**
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	int width, bank;
 
-    // Initialize the result to 0
-    unsigned int result = 0;
+	if (!b)
+		return (0);
 
-    // Loop through each character in the binary string
-    for (int i = 0; i < len; i++) {
-        // Shift the result to the left by 1
-        result <<= 1;
+	i = 0;
 
-        // Check if the current character is '1', if so, set the least significant bit to 1
-        if (binary_str[i] == '1') {
-            result |= 1;
-        }
-    }
+	for (width = 0; b[width] != '\0'; width++)
+		;
 
-    return result;
+	for (width--, bank = 1; width >= 0; width--, bank *= 2)
+	{
+		if (b[width] != '0' && b[width] != '1')
+		{
+			return (0);
+		}
+
+		if (b[width] & 1)
+		{
+			i += bank;
+		}
+	}
+
+	return (i);
 }
-
